11e5caaef0c7a5a6971063459c8ab9c9ee1ad4a3
Hey-bhagwan <ayushupa29@gmail.com>
2025-07-05 20:13:36 +0530

    fix app.listen

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -149,6 +149,7 @@ app.delete('/api/v1/delete-all', async (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
