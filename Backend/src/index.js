87b4447ebbc10635561ab45da084a8043533c5bd
Hey-bhagwan <ayushupa29@gmail.com>
2025-07-06 18:34:13 +0530

    checking backend

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -149,7 +149,7 @@ app.delete('/api/v1/delete-all', async (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
